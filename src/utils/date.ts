01e4ccd33ff6d594bc09fee9aefb66bd4764149f
Creepeevil <hmngoc04072006>
2026-06-08 09:47:16 +0700

    Remove old code

--- src/utils/date.ts
+++
@@ -1,7 +0,0 @@
-export const formatVietnameseDate = (date: string) =>
-  new Intl.DateTimeFormat("vi-VN", {
-    weekday: "long",
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric"
-  }).format(new Date(`${date}T09:00:00+07:00`));
